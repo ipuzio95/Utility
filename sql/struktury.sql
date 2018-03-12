d0920f6aa64388d5ff21d0cf3d2196d85a038a2a
Boguslaw Fries <bfries@dpd.com.pl>
2018-03-12 17:46:32 +0100

    procedura skladowana

--- sql/struktury.sql
+++ sql/struktury.sql
@@ -1,1 +1,19 @@
-call wez_samochody_pracownicze();
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `wez_samochody_pracownicze`()
+BEGIN
+ 
+                CREATE TEMPORARY TABLE sam ENGINE=MEMORY as
+                (
+                               SELECT p.imie, p.nazwisko, p.stanowisko, s.rejestracja
+        FROM pracownicy p
+        LEFT JOIN samochody s ON p.samochod_id = s.id
+                );
+   
+    SELECT imie, nazwisko, stanowisko,
+    CASE WHEN rejestracja IS NULL THEN 'nie ma samochodu' ELSE rejestracja END as rej
+    FROM sam;
+   
+    DROP TEMPORARY TABLE sam;
+ 
+END$$
+DELIMITER ;
